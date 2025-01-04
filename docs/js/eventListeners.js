e231a029befa5759b88fc9672802b52fe4439f7d
Arch <arch1010@icloud.com>
2025-01-04 17:54:23 -0600

    (Feat): Create a version system

--- docs/js/eventListeners.js
+++ docs/js/eventListeners.js
@@ -23,12 +23,22 @@ document.onreadystatechange = () => {
 };
 
 document.addEventListener("mapLoaded", () => {
+	// Adds ease-out transition to the loading screen then removes it.
 	const loadingScreen = document.getElementById("loading-screen");
 	loadingScreen.style.transition = "opacity 1.5s ease-out";
 	loadingScreen.style.opacity = "0";
 	setTimeout(() => {
 		loadingScreen.style.display = "none";
 	}, 1500); // Match the duration of the CSS transition
+
+	// Fetches the version data to store it in our settings
+	fetch("./json/version.json")
+		.then((response) => response.json())
+		.then((data) => {
+			document.getElementById("version-number").textContent = data["number"];
+			document.getElementById("time-updated").textContent = data["time-updated"];
+			document.getElementById("recent-commit").textContent = data["recent-commit"];	
+		})
 });
 
 // Event listener when a variable is updated
